ef743d2b0f1e476c59827993ed818ccf6a54273e
Monish Vassantrai <m.vassantrai@gmail.com>
2025-07-27 19:21:39 +0100

    feat(/pages/transactions): add basic date pickers to select start/end creation transaction dates

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import "react-day-picker/style.css";
 
 import type { JSX } from "react";
 
